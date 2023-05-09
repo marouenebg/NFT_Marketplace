bd53e431452ef2c985da89f72a8ed2d4ad683eb4
marouenebg <marwenbg123@gmail.com>
2023-05-09 22:32:51 +0100

    adding the buy function

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -6,16 +6,23 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 error NftMarketplace_PriceMustBeAboveZero();
 error NftMarketplace_NotApprovedForMarketplace();
 error NftMarketplace_AlreadyListed(address NFTAddress, uint256 tokenId);
+error NftMarketplace_IsNotOwner();
+error NftMarketplace_NotListed(address NFTaddress, uint256 tokenId);
+error NftMarketplace_PriceNotMet(address NFTaddress, uint256 tokenId, uint256 price);
 
 contract NftMarketplace {
-struct Listing {
+struct Listing  {
     uint256 price;
     address seller;
-}
+} 
 event ItemListed(address indexed seller, address indexed NFTAddress, uint256 tokenId, uint256 price);
+event ItemBought(address indexed buyer, address indexed NFTAddress, uint256 tokenId, uint256 price);
 
 //NFT contract address => tokenId => Listing
 mapping(address => mapping(uint256 => Listing)) private s_Listing;
+
+//Seller address to amount earned
+mapping (address => uint256) private s_Proceeds;
 
 modifier NotListed ( address NFTAddress, uint256 tokenId) {
     Listing memory listing = s_Listing[NFTAddress][tokenId] ;
@@ -24,7 +31,31 @@ modifier NotListed ( address NFTAddress, uint256 tokenId) {
     _;
 }
 
-function listItem(address NFTAddress, uint256 tokenId, uint256 price) external {
+modifier IsOwner(address NFTAddress, uint256 tokenId, address spender) {
+    IERC721 nft = IERC721(NFTAddress);
+    address owner = nft.ownerOf(tokenId);
+    if (spender != owner) {
+            revert NftMarketplace_IsNotOwner();
+        }
+    _;   
+ } 
+
+modifier IsListed(address NFTaddress, uint256 tokenId) {
+    Listing memory listing = s_Listing[NFTaddress][tokenId];
+    if ( listing.price <= 0 )  {
+            revert NftMarketplace_NotListed(NFTaddress, tokenId);
+    }
+    _;
+
+}
+/*
+@Notice Method for listing your NFT to the marketplace
+@param NFTaddress: Address of the NFT
+@param tokenId:  the token ID of the NFT
+@param price: the desired price of the NFT
+*/
+function listItem(address NFTAddress, uint256 tokenId, uint256 price) external NotListed(NFTAddress, tokenId)
+    IsOwner(NFTAddress, tokenId, msg.sender) {
     if (price <= 0) 
         revert NftMarketplace_PriceMustBeAboveZero();
     IERC721 nft = IERC721(NFTAddress);
@@ -32,6 +63,17 @@ function listItem(address NFTAddress, uint256 tokenId, uint256 price) external {
         revert NftMarketplace_NotApprovedForMarketplace();
     s_Listing[NFTAddress][tokenId] = Listing(price, msg.sender);
     emit ItemListed(msg.sender, NFTAddress, tokenId, price);
+}
+
+function buyItem(address NFTaddress, uint256 tokenId) external payable IsListed(NFTaddress, tokenId) {
+    Listing memory nftListed = s_Listing[NFTaddress][tokenId] ;
+    if (msg.value < nftListed.price ) {
+        revert NftMarketplace_PriceNotMet(NFTaddress, tokenId, nftListed.price);    
+    }
+    s_Proceeds[nftListed.seller] = s_Proceeds[nftListed.seller] + msg.value;
+    delete (s_Listing[NFTaddress][tokenId]);
+    IERC721(NFTaddress).safeTransferFrom(nftListed.seller, msg.sender, tokenId);
+    emit ItemBought(msg.sender, NFTaddress, tokenId, nftListed.price);
 }
 
 }
