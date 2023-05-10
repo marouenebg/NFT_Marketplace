221b49f9f12980ac70de0eeac86c80e53630ef88
marouenebg <marwenbg123@gmail.com>
2023-05-10 23:15:36 +0100

    Adding the scripts to deploy the contract

---
+++ deploy/01-deploy-NFTMarketplace.js
@@ -0,0 +1,24 @@
+const network = require ("hardhat")
+const {developmentChains} = require ("../helper-hardhat-config")
+const {verify} = require ("../utils/verify")
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deploy, log} = deployments
+    const {deployer} = await getNamedAccounts()
+
+    //args are empty because we don't have a constructor
+    let args = []
+
+    const NftMarketplace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: args, 
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+log("Verifying...")
+//Make sure to add etherscan you your hardhat.config to use verify
+//await verify(NftMarketplace.address, args)
+}
+
+module.exports.tags = ["all", "NFTMarketplace"]
